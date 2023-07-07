72cffa962dee22a7d99fa8496e896dca7f4f2a60
ArS <91518669+arssite@users.noreply.github.com>
2023-07-07 11:43:04 +0530

    The "Bot saves Princess" problem is a classic programming puzzle that involves rescuing a princess trapped in a grid. The problem is often presented as a text-based grid, where the bot and the princess are represented by specific characters.
    The problem statement typically includes the following information:
    
    The size of the grid, denoted by an odd integer N, where N represents the number of rows and columns in the grid.
    The grid itself, where each cell is represented by a specific character. The bot is represented by 'm', the princess by 'p', and empty cells by '-'.
    The objective of the problem is to write a program that determines the shortest path for the bot to reach the princess and rescues her by outputting the sequence of moves the bot should take.

---
+++ botsavesprincess.py
@@ -0,0 +1,51 @@
+def rescue_princess(n, grid):
+    # Find bot 'm' and princess 'p' positions
+    bot_position = None
+    princess_position = None
+
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == 'm':
+                bot_position = (i, j)
+            elif grid[i][j] == 'p':
+                princess_position = (i, j)
+
+    # Determine relative positions and calculate moves
+    moves = []
+    bot_row, bot_col = bot_position
+    princess_row, princess_col = princess_position
+
+    # Move the bot towards the princess
+    while bot_row != princess_row:
+        if bot_row < princess_row:
+            moves.append('DOWN')
+            bot_row += 1
+        else:
+            moves.append('UP')
+            bot_row -= 1
+
+    while bot_col != princess_col:
+        if bot_col < princess_col:
+            moves.append('RIGHT')
+            bot_col += 1
+        else:
+            moves.append('LEFT')
+            bot_col -= 1
+
+    return moves
+
+
+# Get the input from the user
+n = int(input())
+grid = []
+
+#print(f"Enter the {n}x{n} grid:")
+for _ in range(n):
+    row = list(input())
+    grid.append(row)
+
+rescue_moves = rescue_princess(n, grid)
+
+# Print the moves
+for move in rescue_moves:
+    print(move)
